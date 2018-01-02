c914ae6c07ea386b0bea22e2477f489aaec290b5
yunchae.na <ck2204@naver.com>
2018-01-02 18:08:34 +0900

    functional-components

--- src/components/WeatherList.js
+++ src/components/WeatherList.js
@@ -1,23 +1,16 @@
 import React, {Component} from 'react';
 import WeatherListItem from "./WeatherListItem";
 
-class WeatherList extends Component {
-    render() {
-        const {days, onDayClicked} = this.props;
 
-        return (
-            <div className="weather-list flex-parent">
-                {days.map((day, index) =>
-                    <WeatherListItem
-                        key={day.dt}
-                        day={day}
-                        index={index}
-                        onDayClicked={onDayClicked}/>
-                )}
-            </div>
-
-        )
-    }
-}
+const WeatherList = ({days, onDayClicked}) =>
+    <div className="weather-list flex-parent">
+        {days.map((day, index) =>
+            <WeatherListItem
+                key={day.dt}
+                day={day}
+                index={index}
+                onDayClicked={onDayClicked}/>
+        )}
+    </div>
 
 export default WeatherList;
